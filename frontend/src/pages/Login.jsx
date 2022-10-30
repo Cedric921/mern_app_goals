8cb335a377a359d71b5a7e8fdf59b07466dcf474
cedric karungu <ckarungu921@gmail.com>
2022-10-30 16:22:52 +0200

    feat: init auth state

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+
 const Login = () => {
-	return <div>Login</div>;
+	const [user, setUser] = useState({
+		email: '',
+		password: '',
+	});
+
+	const handleChange = (e) => {
+		setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(user);
+	};
+	return (
+		<>
+			<section className='heading'>
+				<h1>Login</h1>
+				<p>Please login an accoun</p>
+			</section>
+			<section className='form'>
+				<form onSubmit={handleSubmit}>
+					<input
+						name='email'
+						type='email'
+						className='form-control'
+						placeholder='Enter your email'
+						onChange={handleChange}
+					/>
+					<input
+						name='password'
+						type='password'
+						className='form-control'
+						placeholder='Enter passowrd'
+						onChange={handleChange}
+					/>
+
+					<button className='btn-control'>login</button>
+				</form>
+			</section>
+		</>
+	);
 };
 
 export default Login;
