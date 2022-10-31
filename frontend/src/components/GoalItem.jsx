e9ef86ea5055d5a686201925921133eda2dd2e0d
cedric karungu <ckarungu921@gmail.com>
2022-10-31 16:17:56 +0200

    style: add style to single goal

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 const GoalItem = ({ goal }) => {
+   const dispatch = useDispatch()
 	return (
 		<div className='goal'>
-			<div>{new Date(goal.createdAt).toLocaleString}</div>
-			<h2>{goal.text}</h2>
+			<div>{new Date(goal.createdAt).toLocaleString()}</div>
+         <h2>{goal.text}</h2>
+         {/* <button className="btn-delete" onClick={dispatch(deleteGoal(goal.id))}>X</button> */}
 		</div>
 	);
 };
